350e86d009c07c946de5ec005384776b1f654ba7
alekhyachowdury <44283926+alekhyachowdury@users.noreply.github.com>
2021-09-24 13:38:07 +0530

    Update deploy1.py
    updated

--- deploy1.py
+++ deploy1.py
@@ -2,12 +2,13 @@ import requests
 import json
 
 url = "https://150.136.152.236:7002/management/wls/latest/deployments/application"
+#url = "http://localhost:1880/test"
 
-payload={"name": "testwebapp","targets": ["ibmjcs_d_adminserver"]}
+payload={"model": {"name":"testwebapp","targets": [ "ibmjcs_d_adminserver"]}}
 
 
-files = {'model':(None, json.dumps(payload), 'application/json'),
-         'deployment': ('SampleWebApp5.war', open('SampleWebApp5.war', 'rb'), 'application/octet-stream')
+files = {
+         'deployment': ('SampleWebApp.war', open('C://deploypoc//SampleWebApp.war', 'rb'), 'application/octet-stream')
                          }
 
 
@@ -16,6 +17,6 @@ headers = {
   'Authorization': 'Basic d2VibG9naWM6d2VsY29tZTE=',
   }
 
-response = requests.request("POST", url, headers=headers,  files=files, verify=False)
+response = requests.request("POST", url, headers=headers, data=payload, files=files, verify=False)
 
 print(response.text)
